2227bd82306fa1314b5d224e216a414931ab3deb
Matias <mdaneri98@gmail.com>
2023-04-09 22:46:45 -0300

    Se agregaró la implementación, todavía con errores, de la shared memory.

--- shared_memory.h
+++ shared_memory.h
@@ -1,10 +1,11 @@
-
+#include <stdlib.h>
 
 #define SHM_NAME "/SO-SHM"
 #define SHM_SEM "/SO-SEM"
 #define SPLIT_TOKEN '&'
 // #define SHM_SIZE 1048576    // 1MB - Se calcula directo en el main process.
 
-void* open_shared_memory(size_t hashes_count, int prot);
-void* write_to_shared_memory(void* shm_ptr, char* buffer, size_t size);
+size_t create_shared_memory(size_t hashes_count);
+void* open_shared_memory(size_t shm_size);
+void write_to_shared_memory(void* shm_ptr, char* buffer, size_t size);
 void unlink_shared_memory_resources();
---
+++ shm_impl.c
@@ -0,0 +1,80 @@
+
+#include <string.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+
+//Shared memory
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+#include <semaphore.h>
+#include "shared_memory.h"
+
+
+size_t create_shared_memory(size_t hashes_count) {
+    //For every hash, a token will be added.
+    size_t shm_size = 16 * (hashes_count + 1);
+
+    //A new shared memory object initially has zero length--the size of the object.
+    int shm_fd = shm_open(SHM_NAME, O_RDWR | O_CREAT, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open");
+        exit(1);
+    }
+    
+    //Cause the regular file named by path or referenced by fd to be truncated to a size of precisely length bytes.
+    if (ftruncate(shm_fd, shm_size) == -1) {
+        perror("ftruncate");
+        exit(1);
+    }
+
+    return shm_size;
+}
+
+void* open_shared_memory(size_t shm_size) {
+    
+    int shm_fd = shm_open(SHM_NAME, O_RDWR | O_CREAT, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open");
+        exit(1);
+    }
+
+    void* shm_ptr = mmap(NULL, shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shm_ptr == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+
+    return shm_ptr;
+}
+
+
+void write_to_shared_memory(void* shm_ptr, char* buffer, size_t size) {
+
+    /* Access to shared memory writing must be done by one slave at a time, so initial
+        value of semaphore has be 1.
+    */
+    sem_t* sem = sem_open(SHM_SEM, O_CREAT, 0666, 1);
+    
+    /*
+        //FIXME
+        Se podría mejorar, teniendo un contador de cuantos hashes ya se guardaron, e ir directo
+        a escribir a la posición proxima a escribir.
+    */
+    sem_wait(sem);  
+    char* lastAppearance = strrchr(shm_ptr, SPLIT_TOKEN);
+    lastAppearance++;
+    strcpy(lastAppearance, buffer);
+    *(lastAppearance + size) = '&';
+    sem_post(sem);
+
+
+    sem_close(sem);
+}
+
+void unlink_shared_memory_resources() {
+}
